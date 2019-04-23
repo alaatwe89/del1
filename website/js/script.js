e21cf5accdd523e784e3fa0ca876aa8ea8dc427f
unknown <you@example.com>
2019-04-23 21:20:19 +0200

    del4uppdate

--- website/js/script.js
+++ website/js/script.js
@@ -48,7 +48,7 @@ app.controller("boxController", function ($rootScope, $http) {
 
 
     $rootScope.sortColumn = "category";
-    // $rootScope.rowLimits = 6;
+    // $rootrootScope.rowLimits = 6;
     $rootScope.rating = function (input) {
         return `${Math.round(((input / starsTotal) * 100) / 10) * 10}%`;
     }
@@ -130,16 +130,78 @@ app.controller("listController", function ($rootScope, $http) {
 
     const starsTotal = 5;
     $rootScope.pagetitle = "list";
-    //$rootScope.rowLimits = 6;
+    //$rootrootScope.rowLimits = 6;
 
 
     $rootScope.sortColumn = "category";
     $rootScope.rating = function (input) {
         return `${Math.round(((input / starsTotal) * 100) / 10) * 10}%`;
+
+
     }
+    var getProductId = function (boxes, id) {
+        return _.find(boxes, function (box) {
+            return box.id === id
+        });
+    };
+
+    $rootScope.cart = [];
+    $rootScope.emptycart = "";
+    $rootScope.addItem = function(box) {
+        var found = getProductId($rootScope.cart, box.id);
+
+        if(found) {
+            found.box += box.quantity;
+        }
+        else {
+            $rootScope.cart.push(angular.copy(box));
+        }
+    }
+    $rootScope.removeItem = function(box) {
+        var index = $rootScope.cart.indexOf(box);
+        $rootScope.cart.splice(index, 1);
+    }
+    $rootScope.getProductCost = function(box) {
+        return box.quantity * box.price;
+    }
+    $rootScope.getProductQuantity = function(box) {
+        return box.quantity;
+    }
+    $rootScope.getTotal = function () {
+        var total = _.reduce($rootScope.cart, function (sum, box) {
+            return sum + $rootScope.getProductCost(box);
+        }, 0);
+
+        if ($rootScope.cart.length === 0) {
+            $rootScope.emptycart = "Your cart is empty."
+        }
+        else {
+            $rootScope.emptycart = ""
+        }
+        return total;
+    }
+    $rootScope.getQuantity = function () {
+        var quantity = _.reduce($rootScope.cart, function (sum, box) {
+            return sum + $rootScope.getProductQuantity(box);
+        }, 0);
+
+        if (quantity === 0) {
+            $rootScope.badgeColor = "badge-secondary"
+        } else {
+            $rootScope.badgeColor = "badge-danger"
+        }
+
+
+        return quantity;
+    }
+
+
+
+ 
+    
 })
 
-app.controller("loginController", function ($rootScope, $location, authService, dialogService) {
+app.controller("loginController", function ($rootrootScope, $location, authService, dialogService) {
     var vm = this;
 
     function login() {
